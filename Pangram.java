62ea918abff821ac7ea796569e0ac6053a36d817
Sarulatha <sarulatha121@gmail.com>
2017-07-16 14:54:50 +0530

    Add files via upload

---
+++ Pangram.java
@@ -0,0 +1,39 @@
+package GuviProgs;
+
+import java.util.Scanner;
+
+public class Pangram {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine().toLowerCase();
+		char[] str1=str.toCharArray();
+		boolean isPangram=true;
+		if(str.length()<26)
+		{
+			isPangram=false;
+		}
+		String alpha="abcdefghijklmnopqrstuvwxyz";
+		for(int i=0;i<str.length();i++)
+		{
+			if(str1[i]>='a'||str1[i]<='z')
+			{
+				isPangram=true;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if(isPangram)
+		{
+		System.out.println("The string is a Pangram");
+		}
+		else
+		{
+			System.out.println("The string is not a Pangram");
+		}
+	}
+
+}
