c5b45aca8fa6b924c42b49087ad3c0dfcbf41578
Sarulatha <sarulatha121@gmail.com>
2017-07-24 15:45:41 +0530

    Add files via upload

---
+++ UnsortedUniqueElement.java
@@ -0,0 +1,46 @@
+package GuviProgs;
+import java.util.*;
+public class UnsortedUniqueElement {
+	static Scanner sc=new Scanner(System.in);
+	public static ArrayList<Integer> list=new ArrayList<Integer>();
+	public void Unique()
+	{
+		System.out.println("Enter the size:");
+		int size=sc.nextInt();
+		System.out.println("Enter the elements:");
+		int []arr=new int[size];
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		int flag=0;
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				if(i!=j)
+				{
+					if(arr[i]!=arr[j])
+					{
+					flag=1;
+					}
+					else
+					{
+						flag=0;
+						break;
+					}
+				}
+			}
+			if(flag==1)
+			{
+				System.out.println(arr[i]);
+			}
+		}
+	}
+	public static void main(String[] args) 
+	{
+		UnsortedUniqueElement unsortedUniqueElement=new UnsortedUniqueElement();
+		unsortedUniqueElement.Unique();
+	}
+
+}
